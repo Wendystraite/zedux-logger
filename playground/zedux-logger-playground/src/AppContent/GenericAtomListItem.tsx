add670f4961d5d6bd8a7be9d90135b6a52b4fc61
Wendy FOUCHTER <wendy.fouchter@gmail.com>
2025-03-06 13:10:19 +0100

    feat(playground): add html title

--- playground/zedux-logger-playground/src/AppContent/GenericAtomListItem.tsx
+++ playground/zedux-logger-playground/src/AppContent/GenericAtomListItem.tsx
@@ -16,59 +16,58 @@ import {
 import { Button } from '../ui/Button';
 import { IconButton } from '../ui/IconButton';
 import { Input } from '../ui/Input';
+import withSuspense from '../utils/withSuspense';
 
-export const GenericAtomListItem = memo(function GenericAtomListItem({
-  atom,
-}: {
-  atom: GenericAtom;
-}) {
-  const genericAtoms = useAtomInstance(genericAtomsAtom);
-  const instance = useAtomInstance(atom.template, []) as AnyAtomInstance;
-  const value = useAtomValue(instance) as unknown;
-  return (
-    <li className="flex flex-col border-b-gray-500 border-dashed border-2 p-2 gap-1">
-      <h2>{instance.id}</h2>
-      <p>
-        value: <span className="text-gray-500">{String(value)}</span>
-      </p>
-      {atom.type === 'number' && (
-        <div className="flex flex-row gap-1">
-          <Button
-            onClick={
-              (instance.exports as ExportsOf<NumberGenericAtom['template']>)
-                .decrement
-            }
-          >
-            -
-          </Button>
-          <Button
-            onClick={
-              (instance.exports as ExportsOf<NumberGenericAtom['template']>)
-                .increment
-            }
-          >
-            +
-          </Button>
-        </div>
-      )}
-      {atom.type === 'text' && (
-        <Input
-          value={value as string}
-          onChange={(value) => {
-            (
-              instance.exports as ExportsOf<TextGenericAtom['template']>
-            ).setText(value);
+export const GenericAtomListItem = memo(
+  withSuspense(function GenericAtomListItem({ atom }: { atom: GenericAtom }) {
+    const genericAtoms = useAtomInstance(genericAtomsAtom);
+    const instance = useAtomInstance(atom.template, []) as AnyAtomInstance;
+    const value = useAtomValue(instance) as unknown;
+    return (
+      <li className="flex flex-col border-b-gray-500 border-dashed border-2 p-2 gap-1">
+        <h2>{instance.id}</h2>
+        <p>
+          value: <span className="text-gray-500">{String(value)}</span>
+        </p>
+        {atom.type === 'number' && (
+          <div className="flex flex-row gap-1">
+            <Button
+              onClick={
+                (instance.exports as ExportsOf<NumberGenericAtom['template']>)
+                  .decrement
+              }
+            >
+              -
+            </Button>
+            <Button
+              onClick={
+                (instance.exports as ExportsOf<NumberGenericAtom['template']>)
+                  .increment
+              }
+            >
+              +
+            </Button>
+          </div>
+        )}
+        {atom.type === 'text' && (
+          <Input
+            value={value as string}
+            onChange={(value) => {
+              (
+                instance.exports as ExportsOf<TextGenericAtom['template']>
+              ).setText(value);
+            }}
+          />
+        )}
+        <IconButton
+          className="size-8 bg-red-500 hover:bg-red-600"
+          onClick={() => {
+            genericAtoms.exports.removeAtom(atom.id);
           }}
-        />
-      )}
-      <IconButton
-        className="size-8 bg-red-500 hover:bg-red-600"
-        onClick={() => {
-          genericAtoms.exports.removeAtom(atom.id);
-        }}
-      >
-        <TrashIcon />
-      </IconButton>
-    </li>
-  );
-});
+        >
+          <TrashIcon />
+        </IconButton>
+      </li>
+    );
+  }),
+);
