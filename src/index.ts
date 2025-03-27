4b66a14be99c0734f2f151eb95488ddf70c2c287
Wendy FOUCHTER <wendy.fouchter@gmail.com>
2025-03-27 20:19:29 +0100

    feat(logger): export more types

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,19 @@ export { addZeduxLogger } from './addZeduxLogger.js';
 export { addBasicZeduxLogger } from './addBasicZeduxLogger.js';
 export { zeduxLoggerAtom } from './zeduxLoggerAtom.js';
 export type { ZeduxLoggerOptions } from './types/ZeduxLoggerOptions.js';
+export type {
+  ZeduxLoggerColors,
+  CompleteZeduxLoggerColors,
+} from './types/ZeduxLoggerColors.js';
+export type {
+  CompleteZeduxLoggerGlobalOptions,
+  ZeduxLoggerGlobalOptions,
+} from './types/ZeduxLoggerGlobalOptions.js';
+export type {
+  CompleteZeduxLoggerLocalOptions,
+  ZeduxLoggerLocalOptions,
+} from './types/ZeduxLoggerLocalOptions.js';
+export type {
+  CompleteZeduxLoggerMergedOptions,
+  ZeduxLoggerMergedOptions,
+} from './types/ZeduxLoggerMergedOptions.js';
